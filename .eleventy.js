cf246cd30a9b63339b08a767b496966121ce4ad9
Will Pak Poy (Surface 2020) <0521858@radford.act.edu.au>
2021-03-09 22:06:43 +1100

    commit changes, footer

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,15 @@
+const pluginSass = require("eleventy-plugin-sass");
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("static");
-  eleventyConfig.addWatchTarget("./static/css/universal.css");
+  eleventyConfig.addWatchTarget("./styles/**/*");
+  eleventyConfig.addPlugin(pluginSass, {sourcemaps: true});
 
   return {
     dir: {
       input: "content",
-      includes: "../_includes"
+      includes: "../_includes",
+      data: "../_data"
     }
   };
 
